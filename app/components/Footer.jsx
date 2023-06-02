fc5c05865496b54f33c9af50de61274b641be6da
lavymeng <lavymeng@gmail.com>
2023-06-02 11:35:10 +0700

    change style and icon

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer self-end text-white bg-zinc-900 px-4 py-2 border-t-2 w-full text-center">
+    <div className="footer self-end text-white bg-slate-600 px-4 py-2 border-t-2 w-full text-center">
       copyright@proadvisor2023
     </div>
   );
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,41 +4,51 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="header m-auto flex justify-center bg-slate-600 text-white w-full ">
+    <header className="header m-auto flex justify-center w-full bg-white drop-shadow-lg">
       <div className="container flex flex-row justify-between mx-5">
         <div className="logo flex justify-center items-center">
           <Image
             src="/Proadvisor - v1-02.png"
             alt="logo"
-            width="72"
-            height="72"
+            width="78"
+            height="78"
           />
         </div>
         <div className="link hidden md:flex items-center md:flex-row flex-col ">
-          <Link href="/" replace className="p-5 hover:bg-lime-900">
+          <Link
+            href="/"
+            replace
+            className="p-5 hover:bg-slate-600 hover:text-white"
+          >
             Home
           </Link>
-          <Link href="/pages/comparisons" className="p-5 hover:bg-lime-900">
+          <Link
+            href="/pages/comparisons"
+            className="p-5 hover:bg-slate-600 hover:text-white"
+          >
             Comparisons
           </Link>
-          <Link href="/pages/resources" className="p-5 hover:bg-lime-900">
+          <Link
+            href="/pages/resources"
+            className="p-5 hover:bg-slate-600 hover:text-white"
+          >
             Resources
           </Link>
           <Link
             href="/pages/promotions"
-            className="p-5 hover:bg-lime-900"
+            className="p-5 hover:bg-slate-600 hover:text-white"
           >
             Promotions
           </Link>
           <Link
             href="/pages/aboutus"
-            className="p-5 hover:bg-lime-900"
+            className="p-5 hover:bg-slate-600 hover:text-white"
           >
             About Us
           </Link>
           <Link
             href="/pages/account"
-            className="p-2 hover:bg-lime-900"
+            className="p-5 hover:bg-slate-600 hover:text-white"
           >
             Account
           </Link>
